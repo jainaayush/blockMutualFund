5d0561558647308f742d587a2966c68d93a6f892
jainaayush <aayushejain147@gmail.com>
2022-08-23 16:46:53 +0530

    removed unused code

--- src/config/api.js
+++ src/config/api.js
@@ -6,7 +6,6 @@ export const getAllCoinsData = async () => {
 }
 
 export const getCoinsDetailsByID = async (modalData) => {
-  debugger
   let result = await Promise.all(
     modalData.data.map(async (item,index) => {
       const result = await axios.get(`v1/cryptocurrency/quotes/latest?id=${item.id}`,{headers: {'X-CMC_PRO_API_KEY':'583eefc0-111d-4fcf-98fe-04b32beecbed'}})
