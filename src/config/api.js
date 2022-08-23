095783712b32f06b895d6f4f55a76e1a69ea61c0
jainaayush <aayushejain147@gmail.com>
2022-08-23 16:52:13 +0530

    removed proxy

--- src/config/api.js
+++ src/config/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 
 export const getAllCoinsData = async () => {
-  const data= await axios.get(`/v1/cryptocurrency/map`,{headers: {'X-CMC_PRO_API_KEY':'583eefc0-111d-4fcf-98fe-04b32beecbed'}})
+  const data= await axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/map`,{headers: {'X-CMC_PRO_API_KEY':'583eefc0-111d-4fcf-98fe-04b32beecbed'}})
   return data.data
 }
 
 export const getCoinsDetailsByID = async (modalData) => {
   let result = await Promise.all(
     modalData.data.map(async (item,index) => {
-      const result = await axios.get(`v1/cryptocurrency/quotes/latest?id=${item.id}`,{headers: {'X-CMC_PRO_API_KEY':'583eefc0-111d-4fcf-98fe-04b32beecbed'}})
+      const result = await axios.get(`https://pro-api.coinmarketcap.comv1/cryptocurrency/quotes/latest?id=${item.id}`,{headers: {'X-CMC_PRO_API_KEY':'583eefc0-111d-4fcf-98fe-04b32beecbed'}})
       return Object.values(result.data.data)[0]
     })
   )
